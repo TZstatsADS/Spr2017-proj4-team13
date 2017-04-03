caa148161a80c5d737d512eec2e731c5698df0e7
bz2290 <bz2290@columbia.edu>
2017-04-03 14:34:42 -0400

    cluster update

--- lib/Spectral Cluster.R
+++ lib/Spectral Cluster.R
@@ -1,53 +1,63 @@
-library(mlbench)
+#library(mlbench)
+
+#set.seed(111)
+#obj = mlbench.spirals(100,1,0.025)
+#my.dat =  4 * obj$x
+#plot(my.dat)
+
 library(stats)
+#This is a function that performs the spectral cluster algorithm
+#Package needed: stats
+#Input: 
+      #my.dat: data matrix
+      #n.line: numerical values indicating how many lines to de drawn for each point
+      #n.cluster: numerical value indeicates the number of clusters
+#output:
+      #km.ouput: A k-means object that contains clluster results
 
-set.seed(111)
-obj <- mlbench.spirals(100,1,0.025)
-my.data <-  4 * obj$x
-plot(my.data)
-
-
-make.affinity <- function(my.data,n.neighboors=2,k=2) {
+Spectral.Cluster = function(my.dat,n.line=2,n.cluster=2) {
   
-  Nrow <- nrow(my.data)
+  Nrow = nrow(my.dat)
   
-  Similar <- matrix(rep(NA,Nrow^2), ncol=Nrow)
+  #Construct Matrix that contains similarity between each values
+  Similar = matrix(rep(NA,Nrow^2), ncol=Nrow)
   
   for(i in 1:Nrow) {
     for(j in 1:Nrow) {
-      Similar[i,j] <- exp(- 1 * norm(as.matrix(my.data[i,]-my.data[j,]), type="F"))
+      Similar[i,j] = exp( -1 * norm(as.matrix(my.dat[i,]-my.dat[j,]), type="F"))
     }
   }
-  #S <- make.similarity(my.data)
   
-  N <- length(Similar[,1])
+  N = length(Similar[,1])
   
-  if (n.neighboors >= N) {  # fully connected
-    Affinity <- Similar
-  } else {
-    Affinity <- matrix(rep(0,N^2), ncol=N)
+  #Construct Affinity matrix
+  Affinity = matrix(rep(0,N^2), ncol=N)
     for(i in 1:N) { # for each line
       # only connect to those points with larger similarity 
-      best.similarities <- sort(Similar[i,], decreasing=TRUE)[1:n.neighboors]
-      for (s in best.similarities) {
-        j <- which(Similar[i,] == s)
-        Affinity[i,j] <- Similar[i,j]
-        Affinity[j,i] <- Similar[i,j] # to make an undirected graph, ie, the matrix becomes symmetric
+      best = sort(Similar[i,], decreasing=TRUE)[1:n.line]
+      for (b in best) {
+        j = which(Similar[i,] == b)
+        Affinity[i,j] = Similar[i,j]
+        Affinity[j,i] = Similar[i,j]
       }
     }
-  }
   
-  D = diag(apply(Affinity,1,sum))
+  #COnstruct degree matrix Degree
+  Degree = diag(apply(Affinity,1,sum))
   
-  U = D - Affinity
-  evL <- eigen(U, symmetric=TRUE)
-  Z   <- evL$vectors[,(ncol(evL$vectors)-k+1):ncol(evL$vectors)]
-  km <- kmeans(Z, centers=k, nstart=5)
+  #Construct unnormalizaed graph Laplacian
+  Lap = Degree - Affinity
   
-  return(km)  
+  ev = eigen(Lap, symmetric=TRUE)#find eigenvectors for the Laplacian
+  Final   = evL$vectors[,(ncol(ev$vectors)-n.cluster+1):ncol(ev$vectors)]#Locate the k smallest eigenvectors to construct final matrix 
+  
+  #perform k-means algorithm to cluster the final matrix
+  km.ouput = kmeans(Final, centers=n.cluster, nstart=5)
+  
+  return(km.ouput)  
 }
 
-A <- make.affinity(my.data,3,2) 
+#A = make.affinity(my.dat,3,2) 
 
 
-plot(my.data, col=A$cluster)
+#plot(my.dat, col=A$cluster)
