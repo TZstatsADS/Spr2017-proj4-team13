fe5bb50885965a31270664500908482761e8b9c3
bz2290 <bz2290@columbia.edu>
2017-04-04 18:04:58 -0400

    a

--- lib/Spectral Cluster.R
+++ lib/Spectral Cluster.R
@@ -8,54 +8,38 @@ my.dat =  4 * obj$x
 #Package needed: stats, expm
 #Input: 
       #my.dat: data matrix
-      #n.line: numerical values indicating how many lines to de drawn for each point
+      #sigma: numerical values indicating bandwidth of RBF kernel
       #n.cluster: numerical value indeicates the number of clusters
-      #A boolean variable indicating whether to calculate a normalized laplacian matrix
 #output:
       #km.ouput: A k-means object that contains clluster results
 
 library(stats)
 library(expm)
 
-Spectral.Cluster = function(my.dat,n.line=3,n.cluster=2,normal = F) {
+Spectral.Cluster = function(my.dat,sigma = 1, n.cluster=2) {
   
   Nrow = nrow(my.dat)
   
-  #Construct Matrix that contains similarity between each values
-  Similar = matrix(rep(NA,Nrow^2), ncol=Nrow)
-  
-  for(i in 1:Nrow) {
-    for(j in 1:Nrow) {
-      Similar[i,j] = exp( -1 * norm(as.matrix(my.dat[i,]-my.dat[j,]), type="F"))
-    }
-  }
-  
-  N = length(Similar[,1])
-  
   #Construct Affinity matrix
-  Affinity = matrix(rep(0,N^2), ncol=N)
-    for(i in 1:N) { # for each line
-      # only connect to those points with larger similarity 
-      best = sort(Similar[i,], decreasing=TRUE)[1:n.line]
-      for (b in best) {
-        j = which(Similar[i,] == b)
-        Affinity[i,j] = Similar[i,j]
-        Affinity[j,i] = Similar[i,j]
-      }
+  Affinity = matrix(rep(0,Nrow^2), ncol=Nrow)
+  for(i in 1 : Nrow){
+    for(j in 1 : Nrow){
+      if(i != j){
+      Affinity[i,j] = exp(-(1/2*(sigma^2)) * norm(as.matrix(my.dat[i,]-my.dat[j,]), type="F")^2)}
     }
+  }  
   
-  #COnstruct degree matrix Degree   #Not sure since this part
+  #COnstruct degree matrix Degree   
   Degree = diag(apply(Affinity,1,sum))
   
-  #Construct unnormalizaed graph Laplacian
-  Lap = Degree - Affinity
-  
-  if(normal){
   #Construct normalizaed graph Laplacian
-  Lap = (Degree %^% (-1/2)) %*% Lap %*% (Degree %^% (-1/2))
-  }
+  Lap = (Degree %^% (-1/2)) %*% Affinity %*% (Degree %^% (-1/2))
   
+  #My parts ended
+  ##########################################################################################################
   ev = eigen(Lap, symmetric=TRUE)#find eigenvectors for the Laplacian
+  
+  
   
   #Final   = ev$vectors[,1:n.cluster]#Locate the n.cluster largest eigenvectors to construct final matrix
   Final   = ev$vectors[,(ncol(ev$vectors)-n.cluster+1):ncol(ev$vectors)]#Locate the n.cluster smallest eigenvectors to construct final matrix 
