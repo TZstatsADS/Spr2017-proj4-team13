e30b705c6f9869ae787e10fa7e16dab0cd746970
ZixuanGuan <zixuan@ZIXUANdeMacBook-Air.local>
2017-04-04 10:41:49 -0400

    change into cvs

--- lib/dataclean.R
+++ lib/dataclean.R
@@ -1,13 +1,9 @@
 setwd("~/Desktop/5243 ADS/proj4/Spr2017-proj4-team13-master/data/nameset")
-#setwd("D:/Columbia University/Spring2017-Applied Data Science/Project_4_Bz2290/Spr2017-proj4-team13/lib")
+
 
 gatherinfo<-function(filename){
   name<-deparse(substitute(filename))
   filename<- read.csv(paste0(name,".txt"), header = F, sep = ">")
-  ##############Possible Modifications#####################
-  #name <- paste0("../data/nameset/",filename,".txt")
-  #filename<- read.csv(name,header = F, sep = ">")
-  #########################################################
   colnames(filename) <- c("Coauthor", "Paper", "Journal")
   filename$AuthorID <- sub("_.*","",filename$Coauthor)
   filename$Coauthor <- gsub("<","",sub("^.*?\\s","", filename$Coauthor))
@@ -20,53 +16,44 @@ gatherinfo<-function(filename){
   return(filename)
 }
 
-
-#################### Modifications ############################
-#Possible modifications....unable to load your Rdata file
-#Please set directory to lib folder in order for the path "../data/" to work
-#AGupta<-gatherinfo("AGupta")
-#AGupta<-as.data.frame(AGupta)
-#save(object = AGupta, file="../data/AGupta.Rdata")
-#load("../data/AGupta.Rdata")
-###############################################################
 AGupta<-gatherinfo(AGupta)
-write.csv(AGupta,"AGupta.Rdata", row.names = F)
+write.csv(AGupta,"AGupta.csv", row.names = F)
 
 AKumar<-gatherinfo(AKumar)
-write.csv(AKumar,"AKumar.Rdata", row.names = F)
+write.csv(AKumar,"AKumar.csv", row.names = F)
 
 CChen<-gatherinfo(CChen)
-write.csv(CChen,"CChen.Rdata", row.names = F)
+write.csv(CChen,"CChen.csv", row.names = F)
 
 DJohnson<-gatherinfo(DJohnson)
-write.csv(DJohnson,"DJohnson.Rdata", row.names = F)
+write.csv(DJohnson,"DJohnson.csv", row.names = F)
 
 JLee<-gatherinfo(JLee)
-write.csv(JLee,"JLee.Rdata", row.names = F)
+write.csv(JLee,"JLee.csv", row.names = F)
 
 JMartin<-gatherinfo(JMartin)
-write.csv(JMartin,"JMartin.Rdata", row.names = F)
+write.csv(JMartin,"JMartin.csv", row.names = F)
 
 JRobinson<-gatherinfo(JRobinson)
-write.csv(JRobinson,"JRobinson.Rdata", row.names = F)
+write.csv(JRobinson,"JRobinson.csv", row.names = F)
 
 JSmith<-gatherinfo(JSmith)
-write.csv(JSmith,"JSmith.Rdata", row.names = F)
+write.csv(JSmith,"JSmith.csv", row.names = F)
 
 KTanaka<-gatherinfo(KTanaka)
-write.csv(KTanaka,"KTanaka.Rdata", row.names = F)
+write.csv(KTanaka,"KTanaka.csv", row.names = F)
 
 MBrown<-gatherinfo(MBrown)
-write.csv(MBrown,"MBrown.Rdata", row.names = F)
+write.csv(MBrown,"MBrown.csv", row.names = F)
 
 MJones<-gatherinfo(MJones)
-write.csv(MJones,"MJones.Rdata", row.names = F)
+write.csv(MJones,"MJones.csv", row.names = F)
 
 MMiller<-gatherinfo(MMiller)
-write.csv(MMiller,"MMiller.Rdata", row.names = F)
+write.csv(MMiller,"MMiller.csv", row.names = F)
 
 SLee<-gatherinfo(SLee)
-write.csv(SLee,"SLee.Rdata", row.names = F)
+write.csv(SLee,"SLee.csv", row.names = F)
 
 YChen<-gatherinfo(YChen)
-write.csv(YChen,"YChen.Rdata", row.names = F)
+write.csv(YChen,"YChen.csv", row.names = F)
