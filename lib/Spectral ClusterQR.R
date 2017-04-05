eefea66099d569018d3f79da7def38a2d793f48b
bz2290 <bz2290@columbia.edu>
2017-04-05 19:06:40 -0400

    up

--- lib/Spectral ClusterQR.R
+++ lib/Spectral ClusterQR.R
@@ -1,28 +1,27 @@
-library(mlbench)
-set.seed(111)
-obj = mlbench.spirals(100,1,0.025)
-my.dat =  4 * obj$x
+#library(mlbench)
+#set.seed(111)
+#obj = mlbench.spirals(100,1,0.025)
+#my.dat =  4 * obj$x
 
 
 #This is a function that performs the spectral cluster algorithm using QR decomposition to assign clusters
 #Package needed:  expm
 #Input: 
 #my.dat: a m by n citation data matrix
-#sigma: numerical values indicating bandwidth of RBF kernel
-#n.cluster: numerical value indeicates the number of clusters
+#n.cluster: numerical value indeicates the number of clusters indicated
 #output:
-#km.ouput: A k-means object that contains clluster results
+#cluster: A numerical vector that contains cluster results for each observation
 
 library(expm)
 
-Spectral.Cluster = function(my.dat,sigma = 1, n.cluster=2) {
+Spectral.Cluster = function(my.dat, n.cluster=2) {
   
   #A m # of features by n # of observations
   
-  Nrow = ncol(my.dat)#Measure number of observations
+  N = ncol(my.dat)#Measure number of observations
   
-  #Construct Affinity matrix
-  Lap = t(my.dat) %*% my.dat
+  #Construct Gram matrix
+  Gram = t(my.dat) %*% my.dat
   
   #for(i in 1 : Nrow){
    # for(j in 1 : Nrow){
@@ -37,9 +36,10 @@ Spectral.Cluster = function(my.dat,sigma = 1, n.cluster=2) {
   #Construct normalizaed graph Laplacian
   #Lap = (Degree %^% (-1/2)) %*% Affinity %*% (Degree %^% (-1/2))
   
-  ev = eigen(Lap, symmetric = TRUE)#Calculate the eigenvectors of Lap
+  #Obtain eigenvctors for the gram matrix
+  ev = eigen(Gram, symmetric = TRUE)
   
-  #Form matrix X by staking the n.cluster largest eigenvectors from Lap
+  #Form matrix X by taking the n.cluster largest eigenvectors from Lap
   X = ev$vectors[,1:n.cluster]
   
   #Renormalizaing each X's rows
@@ -58,12 +58,12 @@ Spectral.Cluster = function(my.dat,sigma = 1, n.cluster=2) {
   #Perform QR decomposition of matrix Y
   Y.decom = qr(Y,LAPACK = TRUE)
   
-  #Obtain R matrix
+  #Obtain R matrix Y.R
   Y.R = qr.R(Y.decom)
   
-  #Obtain the permutaion matrix
+  #Obtain the permutaion matrix Y.P
   column.order = Y.decom$pivot
-  I = diag(nrow = Nrow)
+  I = diag(nrow = N)
   Y.P = I[,column.order]
   
   #Obtain our R_hat matrix specified in the paper
@@ -77,20 +77,20 @@ Spectral.Cluster = function(my.dat,sigma = 1, n.cluster=2) {
   
   return(cluster)  
 }
-cluster = Spectral.Cluster(my.dat)
+#cluster = Spectral.Cluster(my.dat)
 #A = Spectral.Cluster(my.dat,n.line=3,n.cluster=2,normal = F) 
 #B = Spectral.Cluster(my.dat,n.line=3,n.cluster=2,normal = T) 
 
-plot = data.frame(x=my.dat[,1],y=my.dat[,2],class = cluster)
+#plot = data.frame(x=my.dat[,1],y=my.dat[,2],class = cluster)
 
-par(mfrow=c(1,2))
-plot(my.dat,main="Original")
-plot(x = plot$x, y = plot$y, col=plot$class,main="Spectral Cluster with QR")
+#par(mfrow=c(1,2))
+#plot(my.dat,main="Original")
+#plot(x = plot$x, y = plot$y, col=plot$class,main="Spectral Cluster with QR")
 
 
 
-library(stats)
-K = kmeans(my.dat, centers=2,nstart = 5)
-par(mfrow=c(1,2))
-plot(x = plot$x, y = plot$y, col=plot$class,main="Spectral Cluster with QR")
-plot(my.dat, col=K$cluster,main="K means")
+#library(stats)
+#K = kmeans(my.dat, centers=2,nstart = 5)
+#par(mfrow=c(1,2))
+#plot(x = plot$x, y = plot$y, col=plot$class,main="Spectral Cluster with QR")
+#plot(my.dat, col=K$cluster,main="K means")
