9a7bed2a42691083b29abc5212975271b297e5e6
YingxinZhang <yz3032@columbia.edu>
2017-04-08 17:38:32 -0400

    paper 6

--- lib/distance.R
+++ lib/distance.R
@@ -8,7 +8,7 @@ distance <- function(x1,x2,A){
     return(t(x1)%*%A%*%x2)
   }
   numer <- t(x1)%*%A%*%x2
-  denom <- sqrt(Anorm(x1,x1,A))*sqrt(Anorm(x2,x2,A))
+  denom <- sqrt(Anorm(x1,x1,A)*Anorm(x2,x2,A))
   D <- 1-numer/denom
   return(D)
 }
