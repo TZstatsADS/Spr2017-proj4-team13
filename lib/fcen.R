a5d2e4126eccf76ebaff1910c1110ea95bb6439e
YingxinZhang <yz3032@columbia.edu>
2017-04-08 17:39:11 -0400

    paper6

--- lib/fcen.R
+++ lib/fcen.R
@@ -1,7 +1,7 @@
 #fcen(X,cluster,A)
 fcen <- function(X,cluster,A)
 {
-  K <- length(unique(custer))
+  K <- length(unique(cluster))
   centroids <- matrix(NA,nrow=K,ncol = ncol(X))
   for ( i in 1:K)
   {
