c841490ad90343d162a8af5e2b51b67b6075697e
bz2290 <bz2290@columbia.edu>
2017-04-10 12:35:07 -0400

    update

---
+++ lib/Misclassification.R
@@ -0,0 +1,28 @@
+#This function returns our misclasification rate baased on the results returned by different cluster resluts
+Misclassification = function(filename,classify)
+{
+  filename$classify.result = classify
+  filename$cluster.result = NA
+  initial.class = c()
+  Misclassification.rate = 0
+  N = dim(filename)[1] # Total number of observations in filename
+  
+  class = unique(classify)
+  
+  for(i in class)
+  {
+    initial.class = filename$AuthorID[which(filename$classify.result == i)]
+    initial.class = initial.class[order(initial.class,decreasing = FALSE)]
+    filename$cluster.result[which(filename$classify.result==i)] = rep(as.numeric(levels(factor(initial.class))[which.max(tapply(initial.class,factor(initial.class),length))]),length(initial.class))
+  }
+  
+  for(i in 1 : N)
+  {
+    if(filename$AuthorID[i] != filename$cluster.result[i])
+    {
+      Misclassification.rate = Misclassification.rate + 1
+    }
+  }
+  
+  return(list(Nobservation = N, Rate = Misclassification.rate/N))
+}
