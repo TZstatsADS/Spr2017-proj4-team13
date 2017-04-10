d729747fa59cddf275fc02858df20150b8f29bb6
YingxinZhang <yz3032@columbia.edu>
2017-04-09 21:26:45 -0400

    paper6

--- lib/fcen.R
+++ lib/fcen.R
@@ -1,12 +1,24 @@
-#fcen(X,cluster,A)
-fcen <- function(X,cluster,A)
-{
-  K <- length(unique(cluster))
-  centroids <- matrix(NA,nrow=K,ncol = ncol(X))
-  for ( i in 1:K)
-  {
-    Xsub <- X[cluster ==i,]
-    centroids[i,] <- sum(Xsub)/sqrt(Anorm(Xsub,Xsub,A))
+fcen <- function(X,cluster,A){
+  if(is.null(dim(X))){
+    X<-t(as.matrix(X))
   }
+  
+  K <- length(unique(cluster))
+  cluster<-as.numeric(factor(cluster))
+  
+  centroids <- matrix(NA,nrow=K,ncol = ncol(X))
+  
+  for (i in 1:K){ 
+    index<-cluster ==i
+    if (sum(index)>1){
+      Xsub <- X[cluster ==i,]
+      Xsum<-colSums(Xsub)
+      centroids[i,] <- Xsum/sqrt(Anorm(Xsum,Xsum,A))
+    } else {
+      Xsub <- X[cluster ==i,]
+      centroids[i,] <- Xsub/sqrt(Anorm(Xsub,Xsub,A))
+    }
+  }
+    
   return(centroids)
 }
