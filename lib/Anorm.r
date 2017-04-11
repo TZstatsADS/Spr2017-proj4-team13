f0923836abfaa153faf940868a92b929160e42a8
Jihan Wei <jw3447@columbia.edu>
2017-04-11 09:36:23 -0400

    Delete Anorm.r

--- lib/Anorm.r
+++
@@ -1,9 +0,0 @@
-#####################################################################
-### This is the function of calculating norm value under matrix A ###
-### Input: vector, parameter matrix                               ###
-#####################################################################
-
-Anorm <- function(x1,x2,A){
-   return(as.numeric(t(x1)%*%A%*%x2))
-}
-
